ef0341917553674a99f0ce426da70087771a2aae
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-04-20 00:32:45 +0700

    Avoid concatenating undef

--- lib/App/diffdb.pm
+++ lib/App/diffdb.pm
@@ -342,8 +342,8 @@ _
 sub diffdb_sqlite {
     my %args = @_;
 
-    my $dsn1 = "dbi:SQLite:dbname=".delete($args{dbpath1});
-    my $dsn2 = "dbi:SQLite:dbname=".delete($args{dbpath2});
+    my $dsn1 = defined $args{dbpath1} ? "dbi:SQLite:dbname=".delete($args{dbpath1}) : undef;
+    my $dsn2 = defined $args{dbpath2} ? "dbi:SQLite:dbname=".delete($args{dbpath2}) : undef;
     diffdb(
         %args,
         dsn1 => $dsn1,
